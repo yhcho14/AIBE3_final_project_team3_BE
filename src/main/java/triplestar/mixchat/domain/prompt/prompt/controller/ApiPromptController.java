984eccab0049bf795f9a2ed8f98e756d6801b39c
ZeriTime <blee.940604@gmail.com>
2025-11-14 13:04:53 +0900

    fix: 컨트롤러 response 수정

--- src/main/java/triplestar/mixchat/domain/prompt/prompt/controller/ApiPromptController.java
+++ src/main/java/triplestar/mixchat/domain/prompt/prompt/controller/ApiPromptController.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PathVariable;
-import triplestar.mixchat.global.response.ApiResponse;
+import triplestar.mixchat.global.response.CustomResponse;
 import triplestar.mixchat.global.security.CustomUserDetails;
 import triplestar.mixchat.global.springdoc.CommonBadResponse;
 import triplestar.mixchat.global.springdoc.SecurityRequireResponse;
@@ -26,7 +26,7 @@ public interface ApiPromptController {
             summary = "프롬프트 생성", description = "새로운 커스텀 프롬프트를 DB에 저장합니다. (PREMIUM 회원만 가능)"
     )
     @SecurityRequireResponse
-    ApiResponse<Void> create(
+    CustomResponse<Void> create(
             @Parameter(hidden = true) CustomUserDetails userDetails,
             @RequestBody(description = "프롬프트 생성 정보", required = true)
             PromptReq promptReq
@@ -37,7 +37,7 @@ public interface ApiPromptController {
             summary = "프롬프트 수정", description = "본인이 생성한 커스텀 프롬프트를 수정합니다. (PREMIUM 회원만 가능)"
     )
     @SecurityRequireResponse
-    ApiResponse<Void> update(
+    CustomResponse<Void> update(
             @Parameter(hidden = true) CustomUserDetails userDetails,
             @PathVariable Long id,
             @RequestBody(description = "프롬프트 수정 정보", required = true)
@@ -49,7 +49,7 @@ public interface ApiPromptController {
             summary = "프롬프트 삭제", description = "본인이 생성한 커스텀 프롬프트를 삭제합니다. (PREMIUM 회원만 가능)"
     )
     @SecurityRequireResponse
-    ApiResponse<Void> delete(
+    CustomResponse<Void> delete(
             @Parameter(hidden = true) CustomUserDetails userDetails,
             @PathVariable Long id
     );
@@ -59,7 +59,7 @@ public interface ApiPromptController {
             summary = "프롬프트 목록 조회", description = "사용 가능한 프롬프트 목록을 id와 제목만 조회합니다. (회원 등급별로 다름)"
     )
     @SignInInRequireResponse
-    ApiResponse<List<PromptListResp>> list(
+    CustomResponse<List<PromptListResp>> list(
             @Parameter(hidden = true) CustomUserDetails userDetails
     );
 
@@ -68,7 +68,7 @@ public interface ApiPromptController {
             summary = "프롬프트 상세 조회", description = "프리미엄 회원만 자신이 작성한 커스텀 프롬프트 상세정보를 조회할 수 있습니다."
     )
     @SecurityRequireResponse
-    ApiResponse<PromptDetailResp> detail(
+    CustomResponse<PromptDetailResp> detail(
             @Parameter(hidden = true) CustomUserDetails userDetails,
             @PathVariable Long id
     );
--- src/main/java/triplestar/mixchat/domain/prompt/prompt/controller/ApiV1PromptController.java
+++ src/main/java/triplestar/mixchat/domain/prompt/prompt/controller/ApiV1PromptController.java
@@ -7,53 +7,58 @@ import triplestar.mixchat.domain.prompt.prompt.dto.PromptReq;
 import triplestar.mixchat.domain.prompt.prompt.dto.PromptListResp;
 import triplestar.mixchat.domain.prompt.prompt.dto.PromptDetailResp;
 import triplestar.mixchat.domain.prompt.prompt.service.PromptService;
-import triplestar.mixchat.global.response.ApiResponse;
+import triplestar.mixchat.global.response.CustomResponse;
 import triplestar.mixchat.global.security.CustomUserDetails;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/prompt")
 @RequiredArgsConstructor
-public class ApiV1PromptController implements ApiPromptController{
+public class ApiV1PromptController implements ApiPromptController {
     private final PromptService promptService;
 
+    @Override
     @PostMapping("/create")
-    public ApiResponse<Void> create(@AuthenticationPrincipal CustomUserDetails userDetails,
-                                    @RequestBody PromptReq req) {
+    public CustomResponse<Void> create(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                       @RequestBody PromptReq req) {
         Long memberId = userDetails.getId();
         promptService.create(memberId, req);
-        return ApiResponse.ok("프롬프트가 생성되었습니다.");
+        return CustomResponse.ok("프롬프트가 생성되었습니다.");
     }
 
+    @Override
     @PutMapping("/{id}")
-    public ApiResponse<Void> update(@AuthenticationPrincipal CustomUserDetails userDetails,
-                                    @PathVariable Long id,
-                                    @RequestBody PromptReq req) {
+    public CustomResponse<Void> update(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                       @PathVariable Long id,
+                                       @RequestBody PromptReq req) {
         Long memberId = userDetails.getId();
         promptService.update(memberId, id, req);
-        return ApiResponse.ok("프롬프트가 수정되었습니다.");
+        return CustomResponse.ok("프롬프트가 수정되었습니다.");
     }
 
+    @Override
     @DeleteMapping("/{id}")
-    public ApiResponse<Void> delete(@AuthenticationPrincipal CustomUserDetails userDetails,
-                                    @PathVariable Long id) {
+    public CustomResponse<Void> delete(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                       @PathVariable Long id) {
         Long memberId = userDetails.getId();
         promptService.delete(memberId, id);
-        return ApiResponse.ok("프롬프트가 삭제되었습니다.");
+        return CustomResponse.ok("프롬프트가 삭제되었습니다.");
     }
 
+    @Override
     @GetMapping()
-    public ApiResponse<List<PromptListResp>> list(@AuthenticationPrincipal CustomUserDetails userDetails) {
+    public CustomResponse<List<PromptListResp>> list(@AuthenticationPrincipal CustomUserDetails userDetails) {
         Long memberId = userDetails.getId();
         List<PromptListResp> resp = promptService.list(memberId);
-        return ApiResponse.ok("프롬프트 목록 조회 성공", resp);
+        return CustomResponse.ok("프롬프트 목록 조회 성공", resp);
     }
 
+    @Override
     @GetMapping("/{id}")
-    public ApiResponse<PromptDetailResp> detail(@AuthenticationPrincipal CustomUserDetails userDetails,
-                                                @PathVariable Long id) {
+    public CustomResponse<PromptDetailResp> detail(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                   @PathVariable Long id) {
         Long memberId = userDetails.getId();
         PromptDetailResp resp = promptService.detail(memberId, id);
-        return ApiResponse.ok("프롬프트 상세 조회 성공", resp);
+        return CustomResponse.ok("프롬프트 상세 조회 성공", resp);
     }
 }
